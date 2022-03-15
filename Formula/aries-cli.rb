7d840aaa2187e0ca66708ad31adafffec18e0837
morrieinmaas <moritz@animo.id>
2022-03-15 16:05:42 +0100

    feat: wip not what we want but working
    Signed-off-by: morrieinmaas <moritz@animo.id>

--- Formula/aries-cli.rb
+++ Formula/aries-cli.rb
@@ -1,11 +1,15 @@
 class AriesCli < Formula
-  desc "A CLI tool for aries written in Rust"
-  homepage "https://github.com/animo/aries-cli"
-  url "https://github.com/animo/aries-cli/archive/refs/tags/0.2.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "c014f4949a7d191ffde5cdee829d4f8aa0aaacb85551d929171b6c5c40bc0106"
-  version "0.2.0"
- 
+  desc ""
+  homepage ""
+  url "https://drive.google.com/uc?export=download&id=11Wt7s-yPaq8F2TV8uDhcL8MdGnAoJGz6"
+  sha256 "8615e62fd9250480383763c4e5f42896d54013ffcfdc7c4393808e2a24759228"
+  license ""
+
   def install
     bin.install "aries-cli"
+  end
+
+  test do
+    system "true"
   end
 end
