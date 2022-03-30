51401f8c7609b62d488c779e7d43369fe5cf171c
animoNinja <development@animo.id>
2022-03-30 14:02:22 +0000

    A new aries-cli version has been released to homebrew

--- Formula/aries-cli.rb
+++ Formula/aries-cli.rb
@@ -4,13 +4,13 @@ class AriesCli < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url ""
-    sha256 ""
+    url "https://github.com/animo/aries-cli/releases/download/0.1.0/aries-cli-macos-arm https://github.com/animo/aries-cli/releases/download/0.1.0/aries-cli-macos-x86_64"
+    sha256 "7ad424f21d936919cea10a23d7f5f0a8c166d9cb7d5fed269487bcb283f65b3f"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url ""
-    sha256 ""
+    url "https://github.com/animo/aries-cli/releases/download/0.1.0/aries-cli-macos-arm https://github.com/animo/aries-cli/releases/download/0.1.0/aries-cli-macos-x86_64"
+    sha256 "7ad424f21d936919cea10a23d7f5f0a8c166d9cb7d5fed269487bcb283f65b3f"
   end
  
   def install
