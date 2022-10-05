f45b44d3cc3301ff1c7ef0ad051b40d8ad462251
Moriarty <moritz@animo.id>
2022-10-05 10:39:06 +0200

    chore: some feedback address
    Signed-off-by: Moriarty <moritz@animo.id>

--- Formula/agent-cli.rb
+++ Formula/agent-cli.rb
@@ -4,12 +4,12 @@ class Siera < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/animo/siera/releases/download/v0.0.1/apple-x86_64-siera"
+    url "https://github.com/animo/siera/releases/download/v0.1.0/apple-x86_64-siera"
     sha256 "ec38a073534f31f43887c063e7f2e3bad52a064dc5ef023442b8a29f08efc5f7"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/animo/siera/releases/download/v0.0.1/apple-arm-siera"
+    url "https://github.com/animo/siera/releases/download/v0.1.0/apple-arm-siera"
     sha256 "a20fe47ff89ccdaa1f9a0c9e756cc24b0c6fdeef1caed4fa932c2db0a552ca18"
   end
  
