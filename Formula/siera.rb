8959f174d553fd1db8fa6c5b5ab397214747a474
Moriarty <moritz@animo.id>
2023-03-10 12:10:49 +0100

    feat: move repo name to siera-cli
    Signed-off-by: Moriarty <moritz@animo.id>

--- Formula/siera.rb
+++ Formula/siera.rb
@@ -4,12 +4,12 @@ class Siera < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/animo/siera/releases/download/v0.1.0/apple-x86_64-siera"
+    url "https://github.com/animo/siera-cli/releases/download/v0.1.0/apple-x86_64-siera"
     sha256 "3a95f874a8c343dcd46eff3881afd812f6b55ef39d7a57889f43cb3f7e8a69f8"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/animo/siera/releases/download/v0.1.0/apple-arm-siera"
+    url "https://github.com/animo/siera-cli/releases/download/v0.1.0/apple-arm-siera"
     sha256 "6bcf597d2b76cdc504c67f4d96a020b99fee36b73b2d5c4dd56511c33a7a9603"
   end
  
